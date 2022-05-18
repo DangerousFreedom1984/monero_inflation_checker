754b41ad7cb2029ecd7eb7ba3c66320d17d2df2c
DangerousFreedom <dangfreed@tutanota.com>
2022-05-19 01:51:40 +0200

    Modify arguments

--- MIC.py
+++ MIC.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 This work, "MIC - Monero Inflation Checker", is a derivative of:
     "Mininero" by ShenNoether (https://github.com/monero-project/mininero).
@@ -9,61 +10,78 @@ from os.path import exists
 import verify_tx
 import scan_bc
 import settings
+import sys
 
+n = len(sys.argv)
+ 
+if n == 1:
+    print('---------------------------------------')
+    print('WELCOME TO THE MONERO INFLATION CHECKER')
+    print('---------------------------------------')
 
-print('---------------------------------------')
-print('WELCOME TO THE MONERO INFLATION CHECKER')
-print('---------------------------------------')
+    print('Before we start, would you like to use your own full node (faster and more reliable) or use a public node (slower and less reliable)?')
+    print('1. My own full node')
+    print('2. Public node (Seth for Privacy)')
+    val = input("Enter your choice: ")
 
-print('Before we start, would you like to use your own full node (faster and more reliable) or use a public node (slower and less reliable)?')
-print('1. My own full node')
-print('2. Public node (Seth for Privacy)')
-val = input("Enter your choice: ")
-
-if val == '1':
-    settings.node_choice(1)
-else:
-    settings.node_choice(0)
-
-print(' ')
-print('Ok. Done. What do you want to do now?')
-print(' ')
-
-
-print('1. Verify a specific transaction')
-print('2. Scan blockchain')
-print('3. Quit')
-
-
-val = input("Enter your choice: ")
-
-if val == '1':
-    tx_to_check = input('Enter transaction id:')
-    try:
-        str_ki,str_inp, str_out,str_commit= verify_tx.verify_tx(0,[str(tx_to_check)],i_tx=0,details=1)
-        print(''.join(str_ki))
-        print(''.join(str_inp))
-        print(''.join(str_out))
-        print(''.join(str_commit))
-    except KeyError:
-        print('Not found. Please enter a valid transaction.')
-    except Exception:
-        print('Please check if your node is properly running. If so, maybe the verification for this tx was not implemented yet. Please come back later.')
-
-
-elif val == '2':
-    print('Continue scanning...')
-    # import ipdb;ipdb.set_trace()
-    if exists('height.txt'):
-        h = int(scan_bc.read_height())
+    if val == '1':
+        settings.node_choice(1)
     else:
-        h = 0
-        scan_bc.write_height(str(h))
-    scan_bc.start_scanning(h)
+        settings.node_choice(0)
 
-elif val == '3':
-    print('Bye')
+    print(' ')
+    print('Ok. Done. What do you want to do now?')
+    print(' ')
+
+
+    print('1. Verify a specific transaction')
+    print('2. Scan blockchain')
+    print('3. Quit')
+
+
+    val = input("Enter your choice: ")
+
+    if val == '1':
+        tx_to_check = input('Enter transaction id:')
+        try:
+            str_ki,str_inp, str_out,str_commit= verify_tx.verify_tx(0,[str(tx_to_check)],i_tx=0,details=1)
+            print(''.join(str_ki))
+            print(''.join(str_inp))
+            print(''.join(str_out))
+            print(''.join(str_commit))
+        except KeyError:
+            print('Not found. Please enter a valid transaction.')
+        except Exception:
+            print('Please check if your node is properly running. If so, maybe the verification for this tx was not implemented yet. Please come back later.')
+
+
+    elif val == '2':
+        print('Continue scanning...')
+        # import ipdb;ipdb.set_trace()
+        if exists('height.txt'):
+            h = int(scan_bc.read_height())
+        else:
+            h = 0
+            scan_bc.write_height(str(h))
+        scan_bc.start_scanning(h)
+
+    elif val == '3':
+        print('Bye')
+
+    else:
+        print('Option unavailable')
 
 else:
-    print('Option unavailable')
+    if sys.argv[1]=='scan_fast':
+        settings.node_choice(1)
+        print('Continue scanning...')
+        # import ipdb;ipdb.set_trace()
+        if exists('height.txt'):
+            h = int(scan_bc.read_height())
+        else:
+            h = 0
+            scan_bc.write_height(str(h))
+        scan_bc.start_scanning(h)
+    else:
+        print('Unknow argument')
 
